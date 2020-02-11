52f0b3472acf778fe627a5e4c9e7e59574f9be35
arthur-es <arthurcastroes@gmail.com>
2020-02-11 17:51:04 -0300

    header component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
+import Header from './components/Header';
+
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter className="App">
+      <Header />
       <Routes />
       <GlobalStyle />
     </BrowserRouter>
---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Cart } from './styles';
+import logo from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+import { MdShoppingBasket } from 'react-icons/md';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes logo" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+}
