51af7a42622b6829815af8ed8b3974f0b4fc09ec
arthur-es <arthurcastroes@gmail.com>
2020-02-12 18:55:27 -0300

    cart styling

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
+
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-md-runner-2-masculino/58/D12-1407-058/D12-1407-058_zoom1.jpg?ims=544x"
+                alt="tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis massa</strong>
+              <span>R$ 129.90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={15} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
