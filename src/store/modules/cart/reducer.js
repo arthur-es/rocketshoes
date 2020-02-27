9a1bfc11051bbf11dbdc4ce4cfcb3829aca2cca8
Arthur Castro <arthurcastroes@gmail.com>
2020-02-27 13:18:19 -0300

    produto duplicado

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,13 +1,20 @@
+import produce from 'immer';
+
 export default function cart(prevState = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...prevState,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(prevState, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return prevState;
   }
